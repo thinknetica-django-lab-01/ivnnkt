35a27f69e797b9e6ae9fd994f97d70800001852e
Nikita Ivanov <ivnnkt@gmail.com>
2021-01-22 16:02:03 +0300

    Настроен CKEditor, добавленна модель для FlatPages, добавлены urls и запись в admin.py

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
--- e_commerce/main/models.py
+++ e_commerce/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.flatpages.models import FlatPage
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+class NewFlatpage(models.Model):
+    flatpage = models.OneToOneField(FlatPage, on_delete=models.CASCADE)
+    description = RichTextUploadingField(verbose_name='Контент', default='')
+
+    def __str__(self):
+        return self.flatpage.title
+
+    class Meta:
+        verbose_name = "Содержание страницы"
+        verbose_name_plural = "Содержание страницы"
