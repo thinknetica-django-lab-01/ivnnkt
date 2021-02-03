2eb7b1e38c72f72dd7b4c3ce0eb220f12144f00a
Nikita Ivanov <ivnnkt@gmail.com>
2021-02-03 21:26:42 +0300

    добавил проверку возраста

--- e_commerce/main/forms.py
+++ e_commerce/main/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm, IntegerField
-# from django.db import models
+from django.core.exceptions import ValidationError
 from .models import User
 
 
@@ -11,3 +11,12 @@ class ProfileForm(ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'age')
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+
+        #Проверка возраста
+        if data < 18:
+            raise ValidationError('Вам должно быть больше чем 18 лет')
+
+        return data
