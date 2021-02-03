9c1291bea31379794fd1e21ce0122fc222ce3362
Nikita Ivanov <ivnnkt@gmail.com>
2021-02-03 21:19:10 +0300

    добавил поле возраст в модель

--- e_commerce/main/forms.py
+++ e_commerce/main/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, IntegerField
+# from django.db import models
 from .models import User
 
 
 class ProfileForm(ModelForm):
+    age = IntegerField(
+        label='Возраст',
+    )
+
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'email', 'age')
