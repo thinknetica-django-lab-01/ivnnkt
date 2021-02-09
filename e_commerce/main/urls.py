4b1a08dc043f6906fedd0d8a0c7ed54d069fb9d4
Nikita Ivanov <ivnnkt@gmail.com>
2021-02-09 08:03:01 +0300

    allauth отправляет письмо с html содержимым

--- e_commerce/main/urls.py
+++ e_commerce/main/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('goods/', views.ProductListView.as_view(), name='goods'),
     path('goods/<int:pk>/', views.ProductDetailView.as_view(), name='detail_goods'),
-    path('accounts/<int:pk>/profile/', views.ProfileUpdate.as_view(), name="profile"),
+    path('account/<int:pk>/profile/', views.ProfileUpdate.as_view(), name="profile"),
     path('goods/add', views.ProductCreateView.as_view(), name='add_goods'),
     path('goods/edit/<int:pk>/', views.ProductUpdate.as_view(), name="edit_product"),
 ]
