a1533e6cce3057e4530e03010f7753878abc602c
Nikita Ivanov <ivnnkt@gmail.com>
2021-02-11 22:40:11 +0300

    подготовил логику отправки еженедельной рассылки

--- e_commerce/main/models.py
+++ e_commerce/main/models.py
@@ -6,6 +6,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+import datetime
 
 
 class NewFlatpage(models.Model):
@@ -63,6 +64,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     tag = models.ManyToManyField(Tag)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -114,11 +116,31 @@ class Subscriber(models.Model):
         return self.username.username
 
 
-def send_new_product(sender, instance, created, **kwargs):
-    if created:
+# def send_new_product(sender, instance, created, **kwargs):
+#     if created:
+#         sub_list = Subscriber.objects.all()
+#         email = [user.username.email for user in sub_list]
+#         html_content = render_to_string('email_temlates/new.html')
+#         msg = EmailMultiAlternatives(
+#             subject='New in the site',
+#             from_email='from@example.com',
+#             to = email
+#         )
+#         msg.attach_alternative(html_content, "text/html")
+#         msg.send()
+#
+# post_save.connect(send_new_product, sender=Product)
+
+
+def week_new_product(sender, instance, created, **kwargs):
         sub_list = Subscriber.objects.all()
         email = [user.username.email for user in sub_list]
-        html_content = render_to_string('email_temlates/new.html')
+        week_date = datetime.date.today() - datetime.timedelta(days=7)
+        product_list = Product.objects.filter(date__gte=week_date)
+        html_content = render_to_string(
+            'email_temlates/week_new.html',
+            {'product_list': product_list}
+        )
         msg = EmailMultiAlternatives(
             subject='New in the site',
             from_email='from@example.com',
@@ -126,5 +148,3 @@ def send_new_product(sender, instance, created, **kwargs):
         )
         msg.attach_alternative(html_content, "text/html")
         msg.send()
-
-post_save.connect(send_new_product, sender=Product)
