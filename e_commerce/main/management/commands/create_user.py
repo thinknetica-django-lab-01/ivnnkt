5711272bfe0d7743698e56c790d7a9d81e6d27de
Nikita Ivanov <ivnnkt@gmail.com>
2021-03-03 02:04:42 +0300

    поправил команду create_user

--- e_commerce/main/management/commands/create_user.py
+++ e_commerce/main/management/commands/create_user.py
@@ -6,6 +6,24 @@ from django.utils.crypto import get_random_string
 class Command(BaseCommand):
     help = "Создание случайного пользователя"
 
+    def add_arguments(self, parser):
+        parser.add_argument('-n', '--name', type=str, help='username пользователя', )
+
+        parser.add_argument('-p', '--password', type=str, help='пароль пользователя', )
+
     def handle(self, *args, **kwargs):
-        name = get_random_string()
-        User.objects.create_user(username=name, email=f'{name}@mail.net', password='pass1234')
+        name = kwargs['name']
+        password = kwargs['password']
+
+        if name:
+            username = name
+        else:
+            username = get_random_string()
+
+        if password:
+            user_password = password
+        else:
+            user_password = 'default'
+
+        User.objects.create_user(username=username, email=f'{username}@mail.net', password=user_password)
+        self.stdout.write(u'Пользователь успешно создан, login:"%s"!' % (username))
